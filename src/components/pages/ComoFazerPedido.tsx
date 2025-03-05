e38d4f8e830fbd8d7dbc636f9c175e3937dd4120
caaarolb <carolinelboaventura@gmail.com>
2025-03-04 23:08:45 -0300

    ✨ FroAtualização da imagens do modo dark ✨

--- src/components/pages/ComoFazerPedido.tsx
+++ src/components/pages/ComoFazerPedido.tsx
@@ -7,12 +7,12 @@ const ComoFazerPedido = () => {
         </p>
         <div className="grid grid-cols-1 md:grid-cols-4 gap-8 px-6 md:px-20">
           <div className="flex flex-col items-center">
-            <img src="https://i.ibb.co/tMCjn8R6/Fast-Chef-3.png" alt="Pesquisar pela refeição" className="w-50 h-50" />
+            <img src="https://i.ibb.co/Vpj01vM8/Fast-Chef-7.png" alt="Pesquisar pela refeição" className="w-50 h-50" />
             <span className="text-xl font-bold mt-2">01</span>
             <p>Pesquisar pela refeição</p>
           </div>
           <div className="flex flex-col items-center">
-            <img src="https://i.ibb.co/HDM4jTTm/Fast-Chef-4.png" alt="Adicionar ao carrinho" className="w-50 h-50" />
+            <img src="https://i.ibb.co/Gf8WG0fP/FastChef.png" alt="Adicionar ao carrinho" className="w-50 h-50" />
             <span className="text-xl font-bold mt-2">02</span>
             <p>Adicionar ao carrinho</p>
           </div>
