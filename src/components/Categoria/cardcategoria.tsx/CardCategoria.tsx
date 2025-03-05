b25b0ee682bc131a8d6eeeba126c8becda0e455e
gabriel <gabrieldomiciano2017@gmail.com>
2025-03-05 16:13:02 -0300

    feat:implementando logica listar categoria

--- src/components/Categoria/cardcategoria.tsx/CardCategoria.tsx
+++ src/components/Categoria/cardcategoria.tsx/CardCategoria.tsx
@@ -10,7 +10,7 @@ function CardCategoria({categoria}:CardCategoriasProps) {
     return (
         <div className='border rounded-xl p-4 shadow-lg flex items-center justify-between bg-white hover:shadow-xl duration-300'>
     <FaInstagram className="text-4xl text-pink-500 hover:scale-110 duration-200 cursor-pointer ml-2" />
-    <div className='text-2xl font-semibold text-gray-800'>{}</div>
+    <div className='text-2xl font-semibold text-gray-800'>{categoria.nome}</div>
 </div>
     )
 }
--- src/components/Categoria/listarcategorias/ListarCategorias.tsx
+++ src/components/Categoria/listarcategorias/ListarCategorias.tsx
@@ -1,23 +1,59 @@
-import React from 'react'
+import React, { useContext, useEffect, useState } from 'react'
 import CardCategoria from '../cardcategoria.tsx/CardCategoria'
+import { useNavigate } from 'react-router-dom';
+import { buscar } from '../../../services/Service';
+import Categoria from '../../../models/Categoria';
+import { ThreeDots } from 'react-loader-spinner';
 
 function ListarCategorias() {
+  const navigate = useNavigate();
+
+    const [categorias, setCategorias] = useState<Categoria[]>([])
+
+    
+
+    async function buscarCategorias() {
+      try {
+        await buscar("/categorias", setCategorias);
+      } catch (err) {
+        console.log(console.error(err));
+      }
+    }
+  useEffect(() => {
+      buscarCategorias();
+    }, [categorias.length]);
+
   return (
-    <div className=" p-4 bg-red-400 w-full my-4">
+    <>
+
+    <div className=" rounded-lg p-4 bg-blue-400 w-full my-4">
     <h2 className="text-3xl font-bold text-center text-gray-800 mb-6">Categorias</h2>
     <div className="grid grid-cols-1 sm:grid-cols-2 md:grid-cols-3 lg:grid-cols-4 gap-6">
       
-<CardCategoria/>
-<CardCategoria/>
-<CardCategoria/>
-<CardCategoria/>
-<CardCategoria/>
-<CardCategoria/>
+    {categorias.length === 0 && (
+    
+    <ThreeDots
+  visible={true}
+  height="80"
+  width="80"
+  color="#fa7777"
+  radius="9"
+  ariaLabel="three-dots-loading"
+  wrapperStyle={{}}
+  wrapperClass=""
+  
+    />
+      )}
+
+    {categorias.map((categoria) => (
+              <CardCategoria key={categoria.id} categoria={categoria} />
+            ))}
+
 
 
    </div>
    </div>
-   
+   </>
   )
 }
 
