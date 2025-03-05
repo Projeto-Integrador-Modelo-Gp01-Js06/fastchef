9b95c5b12a85acba1b69957715109ef3c15fc523
gabriel <gabrieldomiciano2017@gmail.com>
2025-03-05 15:24:48 -0300

    fix: Atualizando service.ts

--- src/services/Service.ts
+++ src/services/Service.ts
@@ -4,12 +4,7 @@ const api = axios.create({
   baseURL: "https://fastchef.onrender.com/",
 });
 
-export const buscar = async (url: string, setDados: Function) => {
-  const resposta = await api.get(url);
-  setDados(resposta.data);
-};
-
-export const cadastrar = async (
+export const cadastrarUsuario = async (
   url: string,
   dados: Object,
   setDados: Function
@@ -18,15 +13,39 @@ export const cadastrar = async (
   setDados(resposta.data);
 };
 
-export const atualizar = async (
-  url: string,
-  dados: Object,
-  setDados: Function
-) => {
-  const resposta = await api.put(url, dados);
+export const login = async (url: string, dados: Object, setDados: Function) => {
+  const resposta = await api.post(url, dados);
   setDados(resposta.data);
 };
 
-export const deletar = async (url: string) => {
-  await api.delete(url);
+export const buscar = async (
+  url: string,
+  setDados: Function,
+) => {
+  const resposta = await api.get(url);
+  setDados(resposta.data);
 };
+
+export const cadastrar = async (
+  url: string,
+  dados: Object,
+  setDados: Function,
+  header: Object
+) => {
+  const resposta = await api.post(url, dados, header);
+  setDados(resposta.data);
+};
+
+export const atualizar = async (
+  url: string,
+  dados: Object,
+  setDados: Function,
+  header: Object
+) => {
+  const resposta = await api.put(url, dados, header);
+  setDados(resposta.data);
+};
+
+export const deletar = async (url: string, header: Object) => {
+  await api.delete(url, header)
+}
