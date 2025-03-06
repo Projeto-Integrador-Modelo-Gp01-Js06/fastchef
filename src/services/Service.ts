346a56453ce4dbe8d94c97b6b905b4439a64cd98
ustavoteles <telesgustavo.dev@gmail.com>
2025-03-06 13:14:56 -0300

    Ajustes na Service.ts

--- src/services/Service.ts
+++ src/services/Service.ts
@@ -9,19 +9,15 @@ export const cadastrarUsuario = async(url: string, dados: Object, setDados: Func
     setDados(resposta.data)
 }
 
+export const login = async(url: string, dados: Object, setDados: Function) => {
+    const resposta = await api.post(url, dados)
+    setDados(resposta.data)
+}
 
-export const login = async (url: string, dados: Object, setDados: Function) => {
-  const resposta = await api.post(url, dados);
-  setDados(resposta.data);
-};
-
-export const listar = async (
-  url: string,
-  setDados: Function,
-) => {
-  const resposta = await api.get(url);
-  setDados(resposta.data);
-};
+export const listar = async (url: string, setDados: Function, header: Object) =>{
+    const resposta = await api.get(url, header)
+    setDados(resposta.data)
+}
 
 export const cadastrar = async (url: string, dados: Object, setDados: Function, header: Object) =>{
     const resposta = await api.post(url, dados, header)
